05c046f5be2d4705220b340fd88be5a8fe8d0472
Larsanov <ismail.larsanov@epitech.eu>
2020-04-21 12:43:57 +0200

    change stack to column from mainAppController

--- lib/src/mainAppController.dart
+++ lib/src/mainAppController.dart
@@ -13,27 +13,11 @@ class MainAppControllerKosmos extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
+    return Column(
       children: <Widget>[
-        body != null ? Positioned(
-          left: 0,
-          top: 0,
-          right: 0,
-          bottom: 0,
-          child: body,
-        ) : new Container(),
-        appBar != null ? Positioned(
-          top: 0,
-          left: 0,
-          right: 0,
-          child: appBar,
-        ) : new Container(),
-        bottomNavigationBar != null ? Positioned(
-          left: 0,
-          right: 0,
-          bottom: 0,
-          child: bottomNavigationBar,
-        ) : new Container(),
+        body != null ? body : new Container(),
+        appBar != null ? appBar : new Container(),
+        bottomNavigationBar != null ? bottomNavigationBar : new Container(),
       ],
     );
   }
