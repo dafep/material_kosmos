c4f3ee69ce59ec56a6c60ac0ca80a5244a7240cf
Larsanov <ismail.larsanov@epitech.eu>
2020-04-21 13:23:49 +0200

    fdp

--- lib/src/mainAppController.dart
+++ lib/src/mainAppController.dart
@@ -15,18 +15,18 @@ class MainAppControllerKosmos extends StatelessWidget {
   Widget build(BuildContext context) {
     return Stack(
       children: <Widget>[
-        appBar != null ? Positioned(
-          top: 0,
-          left: 0,
-          right: 0,
-          child: appBar,
-        ) : new Container(),
         body != null ? Positioned(
           left: 0,
           top: 0,
           right: 0,
           bottom: 0,
           child: body,
+        ) : new Container(),
+        appBar != null ? Positioned(
+          top: 0,
+          left: 0,
+          right: 0,
+          child: appBar,
         ) : new Container(),
         bottomNavigationBar != null ? Positioned(
           left: 0,
